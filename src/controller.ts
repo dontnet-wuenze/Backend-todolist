548d812b8d299255dedfd7ddb399d4fccec146d9
dotnet <alex0132@163.com>
2020-02-01 17:16:40 +0800

    enable .ts file

--- src/controller.ts
+++ src/controller.ts
@@ -32,7 +32,7 @@ function addMapping(router, mapping) {
 
 function addControllers(router, dir) {
     fs.readdirSync(__dirname + '/' + dir).filter((f) => {
-        return f.endsWith('.js');
+        return f.endsWith('.ts' || '.js');
     }).forEach((f) => {
         console.log(`process controller: ${f}...`);
         let mapping = require(__dirname + '/' + dir + '/' + f);
